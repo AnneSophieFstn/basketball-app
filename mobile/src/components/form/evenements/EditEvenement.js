16ed9a2ef4ceb6cbf9bfcaf4b54435f7b56b09a0
Anne-Sophie <annesophie.faustino@gmail.com>
2023-08-10 22:32:25 +0400

    Vue Evenements

---
+++ mobile/src/components/form/evenements/EditEvenement.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Text, View } from "react-native";
+
+function EditEvenement() {
+  return (
+    <View>
+      <Text>EDIT EVENEMENT</Text>
+    </View>
+  );
+}
+
+export default EditEvenement;
--- mobile/src/screens/evenements/Evenements.js
+++ mobile/src/screens/evenements/Evenements.js
@@ -1,11 +1,60 @@
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, ScrollView } from "react-native";
+import configDB from "../../database/database";
+import CardComponent from "../../components/card/CardComponent";
+import { Button } from "@rneui/themed";
+import { Ionicons } from "@expo/vector-icons";
 
-function Evenements() {
+export default function Evenements({ navigation }) {
+  const [evenements, setEvenements] = useState([]);
+
+  useEffect(() => {
+    configDB.get("/evenements").then((response) => {
+      setEvenements(response.data);
+    });
+  }, []);
+
   return (
     <View>
-      <Text> VUE EVENEMENTS</Text>
+      <Button
+        titleStyle={{ color: "white" }}
+        buttonStyle={{
+          backgroundColor: "red",
+        }}
+        onPress={() => navigation.navigate("Ajouter un evenement")}
+      >
+        <Ionicons
+          name="basketball"
+          size={15}
+          color="#FF9A62"
+          style={{ marginRight: 5 }}
+        />
+        Ajouter un évènement
+      </Button>
+      <ScrollView>
+        <View>
+          {evenements.map((evenement) => (
+            <CardComponent
+              navigation={navigation}
+              key={evenement.id}
+              id={evenement.id}
+              name={evenement.name}
+              type={evenement.type}
+              date={evenement.date}
+              heure={evenement.heure}
+              payant={evenement.payant}
+              description={evenement.description}
+              nbrPlaces={evenement.nbrPlaces}
+              terrain_id={evenement.terrain_id}
+              user_id={evenement.user_id}
+              routeApi="evenements"
+              routeName="VueCard"
+              title="Evenement"
+              titleBtn="PARTICIPER À L'ÉVÈNEMENT"
+            />
+          ))}
+        </View>
+      </ScrollView>
     </View>
   );
 }
-
-export default Evenements;
