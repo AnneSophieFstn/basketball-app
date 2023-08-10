78ba4dd9a3cf7740f60f7d75d2b95c922fc5077c
Anne-Sophie <annesophie.faustino@gmail.com>
2023-08-10 22:33:11 +0400

    Mise en place des routes

--- mobile/src/navigation/Router.js
+++ mobile/src/navigation/Router.js
@@ -5,7 +5,12 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import HomeTabNavigation from "./HomeTabNavigation.js";
-import OneMatch from "../screens/matchs/OneMatch.js";
+import VueCard from "../components/card/VueCard.js";
+import AddMatch from "../components/form/matchs/AddMatch.js";
+import EditMatch from "../components/form/matchs/EditMatch.js";
+import AddEvenement from "../components/form/evenements/AddEvenement.js";
+import EditEvenement from "../components/form/evenements/EditEvenement.js";
+import DeleteComponent from "../components/card/DeleteComponent.js";
 
 const Stack = createNativeStackNavigator();
 
@@ -19,8 +24,36 @@ function Router() {
           options={{ headerShown: false }}
         />
         <Stack.Screen
-          name="Match"
-          component={OneMatch}
+          name="VueCard"
+          component={VueCard}
+          options={{ headerShown: true, headerTitleAlign: "center" }}
+        />
+
+        {/* MATCH */}
+        <Stack.Screen
+          name="Ajouter un match"
+          component={AddMatch}
+          options={{ headerShown: true, headerTitleAlign: "center" }}
+        />
+        <Stack.Screen
+          name="Modifier un match"
+          component={EditMatch}
+          options={{ headerShown: true, headerTitleAlign: "center" }}
+        />
+        {/* EVENEMENT */}
+        <Stack.Screen
+          name="Ajouter un evenement"
+          component={AddEvenement}
+          options={{ headerShown: true, headerTitleAlign: "center" }}
+        />
+        <Stack.Screen
+          name="Modifier un evenement"
+          component={EditEvenement}
+          options={{ headerShown: true, headerTitleAlign: "center" }}
+        />
+        <Stack.Screen
+          name="Supprimer"
+          component={DeleteComponent}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
