d0550d2444370a5bf161af96d1394f97112b39e8
Anne-Sophie <annesophie.faustino@gmail.com>
2023-08-20 22:13:19 +0400

    install package input autocomplete + view

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import Router from "./src/navigation/Router.js";
+import { AutocompleteDropdownContextProvider } from "react-native-autocomplete-dropdown";
 
 export default function App() {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <Router />
+      <AutocompleteDropdownContextProvider>
+        <StatusBar barStyle="dark-content" />
+        <Router />
+      </AutocompleteDropdownContextProvider>
     </>
   );
 }
